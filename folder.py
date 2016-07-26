cd13a5b7e59b160cc088e685118d118789f3e6e7
Eric Briese <ebrie3@gmail.com>
2016-07-26 10:45:19 +1000

    For loop improvement

--- folder.py
+++ folder.py
@@ -2,7 +2,7 @@ import os
 
 # Directory path to wherever you have your folder for the semester.
 # Eg 'C:\Users\Eric\Google Drive\2016 Sem 1'
-folder_directory = r'path_here'
+folder_directory = r'path'
 semester_weeks = 14
 
 # Given a path to a folder, returns all folders within.
@@ -12,10 +12,10 @@ def get_immediate_subdirectories(directory):
 
 # Create a subfolder for each week of the semester for each subject folder.
 subdirs = get_immediate_subdirectories(folder_directory)
-for subdirectory in range(0, len(subdirs) + 1):
+for subdirectory in subdirs:
     for week in range(1, semester_weeks):
         new_folder = 'Week ' + str(week)
-        full_path = folder_directory + '\\' + subdirs[subdirectory] + '\\' + new_folder
+        full_path = folder_directory + '\\' + subdirectory + '\\' + new_folder
         print full_path
         os.makedirs(full_path)
 

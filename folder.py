8693473b100edfa8c850908651d6ee0d3a783b0d
Eric Briese <ebrie3@gmail.com>
2018-12-04 17:53:09 +1000

    Changed folder count logic to be more intuitive

--- folder.py
+++ folder.py
@@ -2,8 +2,8 @@ import os
 
 # Directory path to wherever you have your folder for the semester.
 # Eg 'C:\Users\Eric\Google Drive\2016 Sem 1'
-folder_directory = r'path'
-semester_weeks = 14
+folder_directory = r'F:\GoogleDrive\Summer Semester'
+semester_weeks = 13
 
 # Given a path to a folder, returns all folders within.
 def get_immediate_subdirectories(directory):
@@ -13,7 +13,7 @@ def get_immediate_subdirectories(directory):
 # Create a subfolder for each week of the semester for each subject folder.
 subdirs = get_immediate_subdirectories(folder_directory)
 for subdirectory in subdirs:
-    for week in range(1, semester_weeks):
+    for week in range(1, semester_weeks+1):
         new_folder = 'Week ' + str(week)
         full_path = folder_directory + '\\' + subdirectory + '\\' + new_folder
         print full_path
